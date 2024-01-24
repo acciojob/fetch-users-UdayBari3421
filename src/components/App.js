6709798f6ebda0856b85b267aa62338f579fb11c
UdayBari3421 <udaybari2003@gmail.com>
2024-01-24 19:05:26 +0530

    submitting code-Wed Jan 24 2024 19:05:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,60 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Axios from "axios";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  let [value, setValue] = useState(false);
+  let [data, setData] = useState([]);
 
-export default App
+  function callFunction() {
+    Axios.get("https://reqres.in/api/users")
+      .then((r) => {
+        setValue(true);
+        setData(r.data.data);
+        console.log(r.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div className="container">
+      <div className="box">
+        <h1>Blue Whales</h1>
+        <button onClick={callFunction}>Get User</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(value === true &&
+            data.map((elem) => {
+              return (
+                <tr key={elem.first_name + elem.id}>
+                  <td>{elem.first_name}</td>
+                  <td>{elem.last_name}</td>
+                  <td>{elem.email}</td>
+                  <td>
+                    <img src={elem.avatar} alt="" />
+                  </td>
+                </tr>
+              );
+            })) || (
+            <tr>
+              <td colSpan={"55"}>No result found..</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
