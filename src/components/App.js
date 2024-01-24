b88f567a154bf18a48094cbeb1a173fdcdfd3a8a
UdayBari3421 <udaybari2003@gmail.com>
2024-01-24 19:09:03 +0530

    submitting code-Wed Jan 24 2024 19:09:02 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -50,7 +50,7 @@ const App = () => {
               );
             })) || (
             <tr>
-              <td colSpan={"55"}>No result found..</td>
+              <td colSpan={"55"}>No data found to display.</td>
             </tr>
           )}
         </tbody>
